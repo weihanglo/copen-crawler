b6146d1c8107dd840303bd365f3e02724547ce28
weihang <b01605002@gmail.com>
2016-03-07 16:54:43 +0800

    add PttScraper class

---
+++ connector.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+import threading
+
+class Connector(requests.Session):
+    """Docstring of Connector
+
+        pass
+    """
+
+    def __init__(self, type):
+        super().__init__()
+
+    def search_links(self, url, pattern)
+        pass
+
